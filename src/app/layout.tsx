1d3b4526e591ccf96af0fc5eb19c4f322453eea2
k4rtikay <katt1bh4i@gmail.com>
2025-11-14 13:13:46 +0000

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const inter = Inter({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -25,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${inter.variable} antialiased min-h-screen w-full bg-background text-foreground`}>
+          <div className="
+          absolute inset-0 h-full w-full bg-grid bg-size-[14px_24px] pointer-events-none
+          [mask-image:radial-gradient(ellipse_at_bottom,transparent_25%,black)]"></div>
         {children}
       </body>
     </html>
