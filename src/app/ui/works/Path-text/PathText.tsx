5b9c65b33d4e9ae9a777d26087298e5677f4d894
k4rtikay <katt1bh4i@gmail.com>
2025-11-19 08:32:51 +0000

    feat: add props to allow user to change spacing or copies to fit their string

--- src/app/ui/works/Path-text/PathText.tsx
+++ src/app/ui/works/Path-text/PathText.tsx
@@ -11,9 +11,11 @@ interface PathTextProps{
     speed?: number;
     path?: string;
     className?: string;
+    spacing?: number;
+    copies?: number;
 }
 
-export default function PathText({text,variant,speed=5,className}:PathTextProps){
+export default function PathText({text,variant,speed=5, spacing, copies,className}:PathTextProps){
 
     const pathId = useId()
 
@@ -22,17 +24,22 @@ export default function PathText({text,variant,speed=5,className}:PathTextProps)
             path: "m0,88.5c61.37,0,61.5-68,126.5-68,58,0,51,68,123,68",
             viewBox: "0 0 250 90",
             defaultClass: "w-full",
-            spacing: 40
+            defaultSpacing: 40,
+            defaultCopies: 4,
         },
         circle: {
             path: "M 50, 50 m -40, 0 a 40,40 0 1,1 80,0 a 40,40 0 1,1 -80,0",
             viewBox: "0 0 100 100",
             defaultClass: "w-64 h-64 mx-auto",
-            spacing:50
+            defaultSpacing:50,
+            defaultCopies: 4
         } 
     };
 
-    const { path, viewBox, defaultClass, spacing } = path_config[variant]
+    const { path, viewBox, defaultClass, defaultSpacing, defaultCopies } = path_config[variant]
+
+    const actualSpacing = spacing ?? defaultSpacing;
+    const actualCopies = copies ?? defaultCopies;
 
     return (
         <div className={clsx("overflow-visible", className, defaultClass)}>
@@ -40,13 +47,13 @@ export default function PathText({text,variant,speed=5,className}:PathTextProps)
                 <path id={pathId} fill="none" d={path}/>
                 <text className="text-[8px] uppercase  tracking-wide">
                     {
-                        [...Array(4)].map((_,i)=>{
-                            return (<textPath href={"#"+pathId} key={i} startOffset={i * spacing + "%"} method="stretch">
+                        [...Array(actualCopies)].map((_,i)=>{
+                            return (<textPath href={"#"+pathId} key={i} startOffset={i * actualSpacing + "%"} method="stretch">
                                 {text}
                                 <animate
                                 attributeName="startOffset"
-                                from={i * spacing + "%"}
-                                to={i * spacing - spacing + "%"}
+                                from={i * actualSpacing + "%"}
+                                to={i * actualSpacing - actualSpacing + "%"}
                                 begin={"0s"}
                                 repeatCount={"indefinite"}
                                 dur={`${speed}s`}
