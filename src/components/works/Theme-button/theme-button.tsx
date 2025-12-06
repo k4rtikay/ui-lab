1b2156d2fb7b69eb326b8faa43bcfe1096e9a046
k4rtikay <katt1bh4i@gmail.com>
2025-12-06 12:25:06 +0000

    feat(theme): add functionality to the theme toggle

--- src/components/works/Theme-button/theme-button.tsx
+++ src/components/works/Theme-button/theme-button.tsx
@@ -1,12 +1,25 @@
+"use client";
+
 import { Button } from "@/components/ui/button"
-import { SunMoon } from "lucide-react"
+import { Sun } from "lucide-react"
+import { useTheme } from "next-themes"
 
 export default function ThemeButton(){
+
+    const { theme, setTheme } = useTheme()
+    const isDark:Boolean = theme==="dark"
+
     return (
         <div>
-            <Button variant={"outline"} size={"icon"}>
-                <SunMoon></SunMoon>
+            <Button
+            variant={"outline"}
+            size={"icon"}
+            onClick={()=>{setTheme(isDark? "light": "dark")}}
+            >
+                <Sun></Sun>
+                <span className="sr-only">Toggle theme</span>
             </Button>
         </div>
     )
 }
+
