4ec6d976cc42fb010e19a9f648cd690fed604702
k4rtikay <katt1bh4i@gmail.com>
2025-12-08 07:29:51 +0000

    fix(lenticularCard): fix jittering issue at the border

--- src/components/works/Lenticular-Card/LenticularCard.tsx
+++ src/components/works/Lenticular-Card/LenticularCard.tsx
@@ -7,25 +7,32 @@ export default function LenticularCard() {
     const boundingRef = useRef<DOMRect | null>(null);
 
     return (
-        <div className="flex justify-center perspective-midrange">
+        <div
+            onMouseEnter={(e) => {
+                boundingRef.current = e.currentTarget.getBoundingClientRect();
+            }}
+            onMouseMove={(e) => {
+                if (!boundingRef.current) return;
+                const xPos = e.clientX - boundingRef.current.left;
+                const yPos = e.clientY - boundingRef.current.top;
+                const xPosPercent = xPos / boundingRef.current.width;
+                const yPosPercent = yPos / boundingRef.current.height;
+                const xRotation = (xPosPercent - 0.5) * 25;
+                const yRotation = (0.5 - yPosPercent) * 25;
+
+                e.currentTarget.style.setProperty("--x-rotation", `${xRotation}deg`);
+                e.currentTarget.style.setProperty("--y-rotation", `${yRotation}deg`);
+            }}
+            onMouseLeave={(e)=>{
+                boundingRef.current = null;
+                e.currentTarget.style.setProperty("--x-rotation", "0deg");
+                e.currentTarget.style.setProperty("--y-rotation", "0deg");
+            }}
+
+            className="flex justify-center p-px w-fit perspective-midrange"
+        >
             <div
-                onMouseEnter={(e) => {
-                    boundingRef.current = e.currentTarget.getBoundingClientRect();
-                }}
-                onMouseMove={(e) => {
-                    if (!boundingRef.current) return;
-                    const xPos = e.clientX - boundingRef.current.left;
-                    const yPos = e.clientY - boundingRef.current.top;
-                    const xPosPercent = xPos / boundingRef.current.width;
-                    const yPosPercent = yPos / boundingRef.current.height;
-                    const xRotation = (xPosPercent - 0.5) * 55;
-                    const yRotation = (0.5 - yPosPercent) * 55;
-
-                    e.currentTarget.style.setProperty("--x-rotation", `${xRotation}deg`);
-                    e.currentTarget.style.setProperty("--y-rotation", `${yRotation}deg`);
-                }}
-                className="flex flex-col shadow-lg justify-center width-xl h-[280px] border border-red-200 bg-red-50 p-4 rounded-md text-zinc-900 hover:rotate-x-(--y-rotation) hover:rotate-y-(--x-rotation) hover:scale-110 transition-transform ease-out"
-
+                className="flex flex-col shadow-lg justify-center width-xl h-[280px] border border-red-200 bg-red-50 p-4 rounded-md text-zinc-900 rotate-x-(--y-rotation) rotate-y-(--x-rotation) hover:scale-110 transition-transform ease-out transform-3d"
             >
                 <h1>Lenticular Card</h1>
                 <p>Come on - try hovering on it.</p>
