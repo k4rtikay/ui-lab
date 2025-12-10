157cc739a8541ecf867b72b383d82d4b5aca0c8e
k4rtikay <katt1bh4i@gmail.com>
2025-12-10 11:59:22 +0000

    feat(lenticular-card): add the lenticular transition effect

--- src/app/components/lenticular-card/page.tsx
+++ src/app/components/lenticular-card/page.tsx
@@ -1,14 +1,45 @@
 import LenticularCard from "@/components/works/Lenticular-Card/LenticularCard"
+import Image from "next/image"
 
 export default function LenticularCardPage({
     children,
-}:{children: React.ReactNode}){
+}: { children: React.ReactNode }) {
 
-    return(
+    const layers: React.ReactNode[] = [
+        <Image
+            alt="image of a cyberpunk cityscape during the morning hours"
+            src={"/cyberpunk-sky-morning.png"}
+            width={360}
+            height={240}
+        />,
+
+        <Image
+            alt="image of a cyberpunk cityscape during the noon hours"
+            src={"/cyberpunk-sky-noon.png"}
+            width={360}
+            height={240}
+        />,
+
+        <Image
+            alt="image of a cyberpunk cityscape during the night hours"
+            src={"/cyberpunk-sky-night.png"}
+            width={360}
+            height={240}
+        />
+
+    ]
+
+    return (
         <div className="flex justify-center">
-            <LenticularCard>
-                <h1>Lenticular Card</h1>
-                <p>Come on - try hovering on it.</p>
+            <LenticularCard layers={layers}>
+                <div className="rounded-md shadow-lg overflow-hidden">
+
+                    <div className="px-4 flex flex-col gap-2">
+                        <h1>Lenticular Card</h1>
+                        <p>Come on - try hovering on it.</p>
+                        <p>The images change as you tilt it side to side.</p>
+                    </div>
+                </div>
             </LenticularCard>
         </div>
     )
--- src/components/works/Lenticular-Card/LenticularCard.tsx
+++ src/components/works/Lenticular-Card/LenticularCard.tsx
@@ -6,9 +6,10 @@ import clsx from "clsx";
 interface LenticularcardProps{
     children?: React.ReactNode;
     className?: string;
+    layers: React.ReactNode[];
 }
 
-export default function LenticularCard({children, className} : LenticularcardProps) {
+export default function LenticularCard({children, className, layers} : LenticularcardProps) {
 
     const boundingRef = useRef<DOMRect | null>(null);
 
@@ -23,8 +24,22 @@ export default function LenticularCard({children, className} : LenticularcardPro
                 const yPos = e.clientY - boundingRef.current.top;
                 const xPosPercent = xPos / boundingRef.current.width;
                 const yPosPercent = yPos / boundingRef.current.height;
-                const xRotation = (xPosPercent - 0.5) * 25;
-                const yRotation = (0.5 - yPosPercent) * 25;
+                const xRotation = (xPosPercent - 0.5) * 30;
+                const yRotation = (0.5 - yPosPercent) * 30;
+
+                // Layer 1 (Left): 1 at start, 0 at center
+                const op1 = Math.max(0, 1 - (xPosPercent * 2));
+                
+                // Layer 2 (Center): 0 at edges, 1 at center
+                const op2 = 1 - Math.abs(xPosPercent - 0.5) * 2;
+                
+                // Layer 3 (Right): 0 at center, 1 at end
+                const op3 = Math.max(0, (xPosPercent - 0.5) * 2);
+
+                // 2. Set Variables
+                e.currentTarget.style.setProperty("--op-0", `${op1}`);
+                e.currentTarget.style.setProperty("--op-1", `${op2}`);
+                e.currentTarget.style.setProperty("--op-2", `${op3}`);
 
                 e.currentTarget.style.setProperty("--x-rotation", `${xRotation}deg`);
                 e.currentTarget.style.setProperty("--y-rotation", `${yRotation}deg`);
@@ -33,14 +48,36 @@ export default function LenticularCard({children, className} : LenticularcardPro
                 boundingRef.current = null;
                 e.currentTarget.style.setProperty("--x-rotation", "0deg");
                 e.currentTarget.style.setProperty("--y-rotation", "0deg");
+                e.currentTarget.style.setProperty("--op-0", "0");
+                e.currentTarget.style.setProperty("--op-1", "1");
+                e.currentTarget.style.setProperty("--op-2", "0");
             }}
 
-            className={clsx("flex justify-center p-px w-fit perspective-midrange",className)}
+            className={clsx("p-px perspective-midrange",className)}
         >
             <div
-                className="flex flex-col shadow-lg justify-center width-xl h-[280px] border border-red-200 bg-red-50 p-4 rounded-md text-zinc-900 rotate-x-(--y-rotation) rotate-y-(--x-rotation) hover:scale-110 transition-transform ease-out transform-3d"
+                className="relative rotate-x-(--y-rotation) rotate-y-(--x-rotation) hover:scale-105 transition-transform ease-out transform-3d"
             >
-                {children}   
+                <div>
+                    {
+                        layers.map((item, id)=>{
+                            return (
+                                <div key={id}
+                                className="absolute inset-0"
+                                style={{opacity:`var(--op-${id})`}}>
+                                    {item}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+                <div
+                className="relative h-full flex items-end -z-10"
+                >
+                    {children}
+                </div>
+
             </div>
         </div>
     )
